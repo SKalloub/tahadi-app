8ffcd485cbf352200f147b54bf93e2f55cd8e7c1
Saeed Kalloub <saeed.kalloub@exalt.ps>
2026-06-18 12:09:21 +0300

    New changes

--- src/users.js
+++ src/users.js
@@ -3,5 +3,9 @@ export const ALLOWED_USERS = {
   saeedjk: { name: "سعيد", pass: "s1234", role: "admin" },
   haltam: { name: "حلتم", pass: "h1234", role: "admin" },
   ak70: { name: "هوتر", pass: "a1234", role: "admin" },
+   azmi: { name: "هوتر", pass: "az1234", role: "admin" },
+   
   tareq: { name: "طارق", pass: "t1234", role: "admin" },
+
+
 };
